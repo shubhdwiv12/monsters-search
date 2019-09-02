cd4d35dcb3cbd6d6d8db5a393c12c56173dc40e4
shubhankar Dwivedi <shubhankar.dwivedi3@gmail.com>
2019-09-02 11:30:28 +0530

    Updates

---
+++ precache-manifest.eb5eb5552fdb6fb4287c56a692abe96b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0773de4773300da60c105f1551f4555",
+    "url": "/monsters-search/index.html"
+  },
+  {
+    "revision": "72d15034ae480456d155",
+    "url": "/monsters-search/static/css/main.713fa67e.chunk.css"
+  },
+  {
+    "revision": "cea528bd8a6c6ccbefab",
+    "url": "/monsters-search/static/js/2.9918f956.chunk.js"
+  },
+  {
+    "revision": "72d15034ae480456d155",
+    "url": "/monsters-search/static/js/main.92245a99.chunk.js"
+  },
+  {
+    "revision": "a1e7f4425935b00137d0",
+    "url": "/monsters-search/static/js/runtime~main.ba079d5a.js"
+  }
+]);
